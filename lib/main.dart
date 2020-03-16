75aa915dba118f3fb5962561756ff7f5f46cf270
Lucas Tonussi <lucastonussi@gmail.com>
2020-03-15 23:44:53 -0300

    aula firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,28 @@ import 'package:flutter/material.dart';
 
 void main() async {
   runApp(MyApp());
+//  //Retorna apenas um documento
+//  DocumentSnapshot snapshot = await Firestore.instance.collection(
+//      'mensagens').document('55u5Kw5e3NvGMywiBrnI').get();
+//  print(snapshot.data);
 
-  Firestore.instance
-      .collection('mensagens')
-      .document('ZbkX8BqlHZgHMOmPO5mh')
-      .collection('arquivos')
-      .document()
-      .setData({'nome_arq':'foto.png'});
+//  //Retorna uma fotografia dos dados no momento em que foi disparado a função (Varios documentos).
+//  QuerySnapshot snapshot =
+//      await Firestore.instance.collection('mensagens').getDocuments();
+//  snapshot.documents.forEach((d){
+//    d.reference.updateData({'lido': false});
+//  });
+
+//  //Ler coleções
+//  Firestore.instance.collection('mensagens').snapshots().listen((dado) {
+//    dado.documents.forEach((d) {
+//      print(d.data);
+//    });
+//  });
+
+  Firestore.instance.collection('mensagens').document('55u5Kw5e3NvGMywiBrnI').snapshots().listen((dado){
+    print(dado.data);
+  });
 }
 
 class MyApp extends StatelessWidget {
