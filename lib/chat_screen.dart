7fbda80e9bf39da43509ef431071cb1e5f456b78
Lucas Tonussi <lucastonussi@gmail.com>
2020-04-14 22:50:38 -0300

    chat

--- lib/chat_screen.dart
+++ lib/chat_screen.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
 
+import 'package:chat/chat_message.dart';
 import 'package:chat/text_compose.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class ChatScreen extends StatefulWidget {
   @override
@@ -11,28 +14,146 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
-  void _sendMessage({String text, File imageFile}) async {
-    if(imageFile != null){
-      StorageUploadTask task = FirebaseStorage.instance.ref().child(
-      DateTime.now().millisecondsSinceEpoch.toString()
-      ).putFile(imageFile);
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  FirebaseUser _curretUser;
+  bool _isLoading = false;
+  @override
+  void initState() {
+    super.initState();
+    FirebaseAuth.instance.onAuthStateChanged.listen((user) {
+      setState(() {
+        _curretUser = user;
+      });
+    });
+  }
 
-     StorageTaskSnapshot taskSnapshot = await task.onComplete;
-     String url = await taskSnapshot.ref.getDownloadURL();
-     print(url);
+  Future<FirebaseUser> _getUser() async {
+    if (_curretUser != null) return _curretUser;
+    try {
+      final GoogleSignInAccount googleSignInAccount =
+          await googleSignIn.signIn();
+      //Pega os dados de autenticação do google
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount.authentication;
+      //Conecção com o firebase
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+          idToken: googleSignInAuthentication.idToken,
+          accessToken: googleSignInAuthentication.accessToken);
+      final AuthResult authResult =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+      final FirebaseUser user = authResult.user;
+      return user;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  void _sendMessage({String text, File imageFile}) async {
+    final FirebaseUser user = await _getUser();
+
+    if (user == null) {
+      _scaffoldKey.currentState.showSnackBar(
+        SnackBar(
+          content: Text('Não foi possível fazer o login. Tente novamente'),
+          backgroundColor: Colors.red,
+        ),
+      );
     }
 
-    Firestore.instance.collection('messages').add({'text': text});
+    Map<String, dynamic> data = {
+      'uid': user.uid,
+      'senderName': user.displayName,
+      'senderPhoto': user.photoUrl,
+      'time': Timestamp.now()
+    };
+
+    if (imageFile != null) {
+      StorageUploadTask task = FirebaseStorage.instance
+          .ref()
+          .child(DateTime.now().millisecondsSinceEpoch.toString())
+          .putFile(imageFile);
+
+      setState(() {
+        _isLoading = true;
+      });
+
+      StorageTaskSnapshot taskSnapshot = await task.onComplete;
+      String url = await taskSnapshot.ref.getDownloadURL();
+      data['imgUrl'] = url;
+    }
+
+    if (text != null) {
+      data['text'] = text;
+    }
+    Firestore.instance.collection('messages').add(data);
+
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
-        title: Text('Olá'),
+        centerTitle: true,
+        title: Text(_curretUser != null
+            ? 'Olá, ${_curretUser.displayName}'
+            : 'Chat App'),
         elevation: 0,
+        actions: <Widget>[
+          _curretUser != null
+              ? IconButton(
+                  icon: Icon(Icons.exit_to_app),
+                  onPressed: () {
+                    FirebaseAuth.instance.signOut();
+                    googleSignIn.signOut();
+                    _scaffoldKey.currentState.showSnackBar(
+                      SnackBar(
+                        content: Text('Usuário desconectado.'),
+                      ),
+                    );
+                  })
+              : Container()
+        ],
       ),
-      body: TextComposer(_sendMessage),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: StreamBuilder<QuerySnapshot>(
+                stream: Firestore.instance
+                    .collection('messages')
+                    .orderBy('time')
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.none:
+                    case ConnectionState.waiting:
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    default:
+                      List<DocumentSnapshot> docs =
+                          snapshot.data.documents.reversed.toList();
+                      return ListView.builder(
+                        itemCount: docs.length,
+                        itemBuilder: (context, i) {
+                          return ChatMessage(
+                              docs[i].data,
+                              //O ponto de interrogação trata o nulo para não dar erro
+                              docs[i].data['uid'] == _curretUser?.uid);
+                        },
+                        reverse: true,
+                      );
+                  }
+                }),
+          ),
+          _isLoading ? LinearProgressIndicator() : Container(),
+          TextComposer(_sendMessage),
+        ],
+      ),
     );
   }
 }
